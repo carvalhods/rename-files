0f99f383b5d84a4dfb7bf6b5cd4587949a3c3faa
David Carvalho <david.snt.carvalho@gmail.com>
2017-06-17 20:51:17 -0300

    Add files via upload

---
+++ invert.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -d "$1" ]; then
+  regex='^(.+)( - )(.+)[\.](.+)'
+	cd "$1" 
+  for i in *; do
+  	if [ -f "$i" ]; then
+  		if [[ $i =~ $regex ]]; then
+  			artist="${BASH_REMATCH[1]}"
+  			music="${BASH_REMATCH[3]}"
+  			extension="${BASH_REMATCH[4]}"
+  			newName="$music - $artist.$extension"
+  			mv -v "$i" "$newName"
+  		fi
+  	fi
+  done
+else
+	echo 'Diretório não informado ou não existente'
+fi
